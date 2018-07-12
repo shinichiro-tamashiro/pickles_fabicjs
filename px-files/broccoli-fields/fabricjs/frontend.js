bc4d3c78c1f1a3b17fc5e10cf5484f8db47b2fe6
MARS\shinichiro.tamashiro <shinichiro.tamashiro@imjp.co.jp>
2018-07-12 09:59:49 +0900

    fabricjsで描画まで対応

--- px-files/broccoli-fields/fabricjs/frontend.js
+++ px-files/broccoli-fields/fabricjs/frontend.js
@@ -1,14 +1,30 @@
 window.broccoliFieldFabricjs = function() {
     this.mkEditor = function( mod, data, elm, callback ){
-        var html = '<div><input type="text" class="form-control"></div>'
-                 + '<div style="border:1px solid #ccc;width:100%;height:300px;"><canvas id"canvas"></canvas></div>'
-                 + '';
-        $(elm).html(html);
+        var script = document.createElement('script');
+        script.src = 'file:///C:/xampp/htdocs/pickles_fabricjs/px-files/broccoli-fields/fabricjs/fabric.min.js';
+        document.head.appendChild(script);
+        var link = document.createElement('link');
+        link.href = 'file:///C:/xampp/htdocs/pickles_fabricjs/px-files/broccoli-fields/fabricjs/style.css';
+        link.rel = 'stylesheet';
+        document.head.appendChild(link);
 
-        new Promise(function(rlv){rlv();}).then(function(){ return new Promise(function(rlv, rjt){
-            callback();
-        }); });
+        script.onload = function(){
+            var canvasfield = '<div class="drowing">'
+                            + '<canvas id="drowarea"></canvas>'
+                            + '</div>';
+            $(elm).html(canvasfield);
 
-        return this;
+            var drowarea = new fabric.Canvas('drowarea', {
+                width: 500,
+                height: 500,
+                isDrawingMode: true
+            });
+
+            new Promise(function(rlv){rlv();}).then(function(){ return new Promise(function(rlv, rjt){
+                callback();
+            }); });
+
+            return this;
+        };
     };
 }
