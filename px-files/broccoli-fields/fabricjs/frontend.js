be21878f67ed5ce6cc54d942c203864ea16f2e11
MARS\shinichiro.tamashiro <shinichiro.tamashiro@imjp.co.jp>
2018-07-13 13:15:50 +0900

    タグ読み込み時の動作　修正中

--- px-files/broccoli-fields/fabricjs/frontend.js
+++ px-files/broccoli-fields/fabricjs/frontend.js
@@ -1,32 +1,19 @@
 window.broccoliFieldFabricjs = function() {
     this.mkEditor = function( mod, data, elm, callback ){
-        var loadFabricjs = function(){
-            /*var script = document.createElement('script');
-            script.src = 'file:///C:/xampp/htdocs/pickles_fabricjs/px-files/broccoli-fields/fabricjs/fabric.min.js';
-            document.head.appendChild(script);*/
-            $.ajax({
-                url: 'file:///C:/xampp/htdocs/pickles_fabricjs/px-files/broccoli-fields/fabricjs/fabric.min.js',
-                dataType: 'script'
-            })
-            .done(function(){console.log('script')});
+        var script = document.createElement('script');
+        script.src = 'file:///C:/xampp/htdocs/pickles_fabricjs/px-files/broccoli-fields/fabricjs/fabric.min.js';
+        script.onload = function(){
+            var drawarea = new fabric.Canvas('drawarea', {
+                width: 700,
+                height: 500
+            });
+            //document.head.appendChild(link);
         };
 
-        var loadCss = function(){
-            /*var link = document.createElement('link');
-            link.href = 'file:///C:/xampp/htdocs/pickles_fabricjs/px-files/broccoli-fields/fabricjs/style.css';
-            link.rel = 'stylesheet';
-            document.head.appendChild(link);*/
-            $.ajax({
-                url: 'file:///C:/xampp/htdocs/pickles_fabricjs/px-files/broccoli-fields/fabricjs/style.css',
-                dataType: 'css'
-            })
-            .done(function(){console.log('css')})
-            .fail(function(e){console.log(e)});
-        };
-
-        $.when(loadFabricjs(), loadCss())
-        .done(function(){
-            console.log(typeof fabric);
+        var link = document.createElement('link');
+        link.href = 'file:///C:/xampp/htdocs/pickles_fabricjs/px-files/broccoli-fields/fabricjs/style.css';
+        link.rel = 'stylesheet';
+        link.onload = function(){
             var canvasfield = '<div class="drawing">'
                             + '<canvas id="drawarea"></canvas>'
                             + '<div><button id="btnDraw">描画</button><button id="btnDrawEnd">描画終了</button></div>'
@@ -35,40 +22,41 @@ window.broccoliFieldFabricjs = function() {
                             + '</div>';
             $(elm).html(canvasfield);
 
-            var drawarea = new fabric.Canvas('drawarea', {
-                width: 700,
-                height: 500
-            });
-            console.log('done3333333');
+            
+        };
+        document.head.appendChild(link);
+        document.head.appendChild(script);
 
-            $(document).on('click', '#btnDraw', function(){
-                drawarea.isDrawingMode = true;
-                return false;
-            });
 
-            $(document).on('click', '#btnDrawEnd', function(){
-                drawarea.isDrawingMode = false;
-                return false;
-            });
-
-            //線の色指定
-            $(document).on('change', '#btnColor', function(){
-                drawarea.freeDrawingBrush.color = this.value;
-            });
-
-            //線幅指定
-            $(document).on('change', '#lineWidth', function(){
-                drawarea.freeDrawingBrush.width = parseInt(this.value, 10) || 1;
-                $('.widthInfo').html(this.value);
-            });
-
-            new Promise(function(rlv){rlv();}).then(function(){ return new Promise(function(rlv, rjt){
-                callback();
-            }); });
-
-            return this;
+        //描画ボタン
+        $(document).on('click', '#btnDraw', function(){
+            drawarea.isDrawingMode = true;
+            return false;
         });
-        
+
+        //描画終了ボタン
+        $(document).on('click', '#btnDrawEnd', function(){
+            drawarea.isDrawingMode = false;
+            return false;
+        });
+
+        //線の色指定
+        $(document).on('change', '#btnColor', function(){
+            drawarea.freeDrawingBrush.color = this.value;
+        });
+
+        //線幅指定
+        $(document).on('change', '#lineWidth', function(){
+            drawarea.freeDrawingBrush.width = parseInt(this.value, 10) || 1;
+            $('.widthInfo').html(this.value);
+        });
+
+        new Promise(function(rlv){rlv();}).then(function(){ return new Promise(function(rlv, rjt){
+            callback();
+        }); });
+
+        return this;
+
 
         /*link.onload = function(){
             var canvasfield = '<div class="drawing">'
